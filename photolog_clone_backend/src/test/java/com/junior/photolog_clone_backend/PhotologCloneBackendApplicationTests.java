6f3e5eb432a65bce1f58bca257336cdc3333a360
TheperZ <94631956+TheperZ@users.noreply.github.com>
2024-09-05 20:21:52 +0900

    init project

---
+++ photolog_clone_backend/src/test/java/com/junior/photolog_clone_backend/PhotologCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.photolog_clone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotologCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
