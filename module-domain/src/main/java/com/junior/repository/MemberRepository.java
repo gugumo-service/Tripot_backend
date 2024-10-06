3f114b1369b357e7920aa889a4b54532c82b3e1a
TheperZ <94631956+TheperZ@users.noreply.github.com>
2024-10-07 02:35:03 +0900

    회원 조회 함수 리턴 타입 optional로 변경

--- module-domain/src/main/java/com/junior/repository/MemberRepository.java
+++ module-domain/src/main/java/com/junior/repository/MemberRepository.java
@@ -3,9 +3,11 @@ package com.junior.repository;
 import com.junior.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByUsername(String username);
 
-    Member findByUsername(String username);
+    Optional<Member> findByUsername(String username);
 }
