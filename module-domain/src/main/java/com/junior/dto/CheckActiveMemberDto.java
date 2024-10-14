a183b3d2c78a5327122885cce0b494eef6a022b0
TheperZ <ckdgh7884@gmail.com>
2024-10-15 01:01:57 +0900

    회원 가입시 나타나는 DTO 필드명 수정
    - hasToAdd -> isActivate

--- module-domain/src/main/java/com/junior/dto/CheckActiveMemberDto.java
+++ module-domain/src/main/java/com/junior/dto/CheckActiveMemberDto.java
@@ -1,12 +1,9 @@
 package com.junior.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
 
 public record CheckActiveMemberDto (
         String nickname,
-        Boolean hasToAdd
+        Boolean isActivate
 
 )
 {
