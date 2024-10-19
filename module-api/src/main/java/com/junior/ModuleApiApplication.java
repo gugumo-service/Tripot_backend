d96c2acd0ce9984ed2eae11a1eeaf50d6390f94f
TheperZ <ckdgh7884@gmail.com>
2024-10-20 04:16:19 +0900

    yaml 중복 등록 제거

--- module-api/src/main/java/com/junior/ModuleApiApplication.java
+++ module-api/src/main/java/com/junior/ModuleApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
-        System.setProperty("spring.config.name", "application-api,application-domain,application-common");
         SpringApplication.run(ModuleApiApplication.class, args);
     }
 }
