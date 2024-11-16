a75cdae1c9c2d060e977d8f80741af0f580f57fb
hspark <donald010@naver.com>
2024-11-16 16:32:47 +0900

    refactoring
    story -> ResponseStoryDto 변환 코드 추가

--- module-domain/src/main/java/com/junior/dto/story/ResponseStoryDto.java
+++ module-domain/src/main/java/com/junior/dto/story/ResponseStoryDto.java
@@ -1,8 +1,12 @@
 package com.junior.dto.story;
 
+import com.junior.domain.story.Story;
 import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 
+@Builder
 public record ResponseStoryDto(
         Long id,
         String title,
@@ -27,5 +31,19 @@ public record ResponseStoryDto(
         this.likeCnt = likeCnt;
         this.isHidden = isHidden;
         this.createDate = createDate;
+    }
+
+    public static ResponseStoryDto from(Story story) {
+        return ResponseStoryDto.builder()
+                .id(story.getId())
+                .title(story.getTitle())
+                .content(story.getContent())
+                .thumbnailImg(story.getThumbnailImg())
+                .latitude(story.getLatitude())
+                .longitude(story.getLongitude())
+                .city(story.getCity())
+                .likeCnt(story.getLikeCnt())
+                .isHidden(story.isHidden())
+                .build();
     }
 }
