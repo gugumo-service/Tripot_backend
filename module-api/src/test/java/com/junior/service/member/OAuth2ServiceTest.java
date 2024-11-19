66cd9eaa9030f902d6d605ac51b3980f0226281a
TheperZ <ckdgh7884@gmail.com>
2024-11-20 02:59:10 +0900

    oauth2Service 단위 테스트코드 작성

--- module-api/src/test/java/com/junior/service/member/OAuth2ServiceTest.java
+++ module-api/src/test/java/com/junior/service/member/OAuth2ServiceTest.java
@@ -5,14 +5,18 @@ import com.junior.domain.member.MemberRole;
 import com.junior.domain.member.MemberStatus;
 import com.junior.domain.member.SignUpType;
 import com.junior.dto.jwt.LoginCreateJwtDto;
+import com.junior.dto.jwt.RefreshTokenDto;
 import com.junior.dto.member.CheckActiveMemberDto;
 import com.junior.dto.oauth2.OAuth2Provider;
 import com.junior.dto.oauth2.OAuth2UserInfo;
+import com.junior.exception.JwtErrorException;
 import com.junior.repository.member.MemberRepository;
 import com.junior.security.JwtUtil;
 import com.junior.strategy.oauth2.KakaoOAuth2LoginStrategy;
 import com.junior.strategy.oauth2.OAuth2UserGenerator;
 import com.junior.util.RedisUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,8 +28,10 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class OAuth2ServiceTest {
@@ -68,8 +74,8 @@ class OAuth2ServiceTest {
         //then
 
         //토큰이 헤더에 정상적으로 들어가야 함
-        assertThat(response.getHeader("Authorization")).isEqualTo("Bearer "+sampleAccess);
-        assertThat(response.getHeader("refresh_token")).isEqualTo("Bearer "+sampleRefresh);
+        assertThat(response.getHeader("Authorization")).isEqualTo("Bearer " + sampleAccess);
+        assertThat(response.getHeader("refresh_token")).isEqualTo("Bearer " + sampleRefresh);
 
         //새 회원이므로 isActivate는 false여야 함
         assertThat(result.nickname()).isEqualTo("nickname");
@@ -107,8 +113,8 @@ class OAuth2ServiceTest {
         //then
 
         //토큰이 헤더에 정상적으로 들어가야 함
-        assertThat(response.getHeader("Authorization")).isEqualTo("Bearer "+sampleAccess);
-        assertThat(response.getHeader("refresh_token")).isEqualTo("Bearer "+sampleRefresh);
+        assertThat(response.getHeader("Authorization")).isEqualTo("Bearer " + sampleAccess);
+        assertThat(response.getHeader("refresh_token")).isEqualTo("Bearer " + sampleRefresh);
 
         //새 회원이므로 isActivate는 false여야 함
         assertThat(result.nickname()).isEqualTo("nickname");
@@ -146,8 +152,8 @@ class OAuth2ServiceTest {
         //then
 
         //토큰이 헤더에 정상적으로 들어가야 함
-        assertThat(response.getHeader("Authorization")).isEqualTo("Bearer "+sampleAccess);
-        assertThat(response.getHeader("refresh_token")).isEqualTo("Bearer "+sampleRefresh);
+        assertThat(response.getHeader("Authorization")).isEqualTo("Bearer " + sampleAccess);
+        assertThat(response.getHeader("refresh_token")).isEqualTo("Bearer " + sampleRefresh);
 
         //새 회원이므로 isActivate는 false여야 함
         assertThat(result.nickname()).isEqualTo("nickname");
@@ -155,10 +161,116 @@ class OAuth2ServiceTest {
     }
 
 
+    @Test
+    @DisplayName("로그아웃의 모든 과정이 성공적으로 진행되어야 함")
+    void logout_success() {
+
+        //given
+
+        //해당 토큰의 유효기간은 테스트 메서드 작동으로부터 6개월
+        String refreshToken = "Bearer test_refresh";
+        String realRefresh = refreshToken.split(" ")[1];
+
+        RefreshTokenDto refreshTokenDto = new RefreshTokenDto(refreshToken);
+
+        given(jwtUtil.getCategory(realRefresh)).willReturn("refresh");
+
+        //when
+        oAuth2Service.logout(refreshTokenDto);
+
+        //then
+        verify(redisUtil).deleteData(realRefresh);     //해당 메서드가 동작하였는지 검증
+    }
 
     @Test
-    void logout() {
+    @DisplayName("로그아웃 중 유효한 토큰이 아닐 경우(RFC 7235를 지키지 않음) 관련 예외처리를 해야 함")
+    void logout_not_valid() {
+
+        //given
+
+        //해당 토큰의 유효기간은 테스트 메서드 작동으로부터 6개월
+        String refreshToken = "Beareer test_refresh";
+        String realRefresh = refreshToken.split(" ")[1];
+
+        RefreshTokenDto refreshTokenDto = new RefreshTokenDto(refreshToken);
+
+
+        //when, then
+        assertThatThrownBy(() -> oAuth2Service.logout(refreshTokenDto))
+                .isInstanceOf(JwtErrorException.class)
+                .hasMessageContaining("유효하지 않은 토큰");
 
 
     }
+
+    @Test
+    @DisplayName("로그아웃 중 refresh 토큰이 아닐 경우 관련 예외처리를 해야 함")
+    void logout_not_refresh() {
+
+        //given
+
+        //해당 토큰의 유효기간은 테스트 메서드 작동으로부터 6개월
+        String accessToken = "Bearer test_access";
+        String realAccess = accessToken.split(" ")[1];
+
+        RefreshTokenDto refreshTokenDto = new RefreshTokenDto(accessToken);
+
+        given(jwtUtil.getCategory(realAccess)).willReturn("access");
+
+        //when, then
+        assertThatThrownBy(() -> oAuth2Service.logout(refreshTokenDto))
+                .isInstanceOf(JwtErrorException.class)
+                .hasMessageContaining("Refresh token이 아님");
+
+
+    }
+
+    @Test
+    @DisplayName("로그아웃 중 토큰이 만료되었을 경우 관련 예외처리를 해야 함")
+    void logout_expired() {
+
+
+        //given
+
+        //해당 토큰의 유효기간은 테스트 메서드 작동으로부터 6개월
+        String refreshToken = "Bearer test_refresh";
+        String realRefresh = refreshToken.split(" ")[1];
+
+        RefreshTokenDto refreshTokenDto = new RefreshTokenDto(refreshToken);
+
+        given(jwtUtil.getCategory(realRefresh)).willThrow(ExpiredJwtException.class);
+
+
+        //when, then
+        assertThatThrownBy(() -> oAuth2Service.logout(refreshTokenDto))
+                .isInstanceOf(JwtErrorException.class)
+                .hasMessageContaining("만료된 Refresh 토큰");
+
+
+    }
+
+    @Test
+    @DisplayName("로그아웃 중 유효하지 않은 토큰에 대해 관련 예외처리를 해야 함")
+    void logout_jwt_exception() {
+
+
+        //given
+
+        //해당 토큰의 유효기간은 테스트 메서드 작동으로부터 6개월
+        String refreshToken = "Bearer test_refresh";
+        String realRefresh = refreshToken.split(" ")[1];
+
+        RefreshTokenDto refreshTokenDto = new RefreshTokenDto(refreshToken);
+
+        given(jwtUtil.getCategory(realRefresh)).willThrow(JwtException.class);
+
+
+        //when, then
+        assertThatThrownBy(() -> oAuth2Service.logout(refreshTokenDto))
+                .isInstanceOf(JwtErrorException.class)
+                .hasMessageContaining("유효하지 않은 토큰");
+
+
+    }
+
 }
