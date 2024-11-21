0c357ebd7c020fc7133152657eaf52e7ebd7466b
TheperZ <ckdgh7884@gmail.com>
2024-11-22 04:17:17 +0900

    공지사항 조회 관련 API 작성

--- module-api/src/main/java/com/junior/controller/api/NoticeApi.java
+++ module-api/src/main/java/com/junior/controller/api/NoticeApi.java
@@ -1,19 +1,25 @@
 package com.junior.controller.api;
 
 import com.junior.dto.admin.notice.CreateNoticeDto;
+import com.junior.dto.admin.notice.NoticeDetailDto;
+import com.junior.dto.admin.notice.NoticeDto;
 import com.junior.dto.admin.notice.UpdateNoticeDto;
+import com.junior.page.PageCustom;
 import com.junior.response.CommonResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface NoticeApi {
 
-    @Operation(summary = "회원 활성화", description = "회원의 추가정보를 입력받습니다.",
+    @Operation(summary = "공지사항 업로드", description = "공지사항을 업로드합니다.",
             responses = {
                     @ApiResponse(responseCode = "201", description = "공지사항 업로드 성공",
                             content = @Content(mediaType = "application/json", schema = @Schema(implementation = CommonResponse.class),
@@ -29,6 +35,106 @@ public interface NoticeApi {
                                     )))
             })
     public CommonResponse<Object> saveNotice(@RequestBody CreateNoticeDto createNoticeDto);
+
+
+    @Operation(summary = "공지사항 조회", description = "공지사항을 조회합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "공지사항 정보",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CommonResponse.class),
+                                    examples = @ExampleObject(
+                                            value = """
+                                                    {
+                                                         "customCode": "NOTICE-SUCCESS-004",
+                                                         "customMessage": "공지사항 조회 성공",
+                                                         "status": true,
+                                                         "data": {
+                                                             "content": [
+                                                                 {
+                                                                     "id": 3,
+                                                                     "title": "타이틀"
+                                                                 },
+                                                                 {
+                                                                     "id": 2,
+                                                                     "title": "타이틀"
+                                                                 },
+                                                                 {
+                                                                     "id": 1,
+                                                                     "title": "타이틀"
+                                                                 }
+                                                             ],
+                                                             "pageable": {
+                                                                 "number": 3,
+                                                                 "size": 15,
+                                                                 "sort": {
+                                                                     "empty": true,
+                                                                     "unsorted": true,
+                                                                     "sorted": false
+                                                                 },
+                                                                 "first": false,
+                                                                 "last": true,
+                                                                 "hasNext": false,
+                                                                 "totalPages": 3,
+                                                                 "totalElements": 33,
+                                                                 "numberOfElements": 3,
+                                                                 "empty": false
+                                                             }
+                                                         }
+                                                     }
+                                                    """
+                                    )))
+            })
+    public CommonResponse<PageCustom<NoticeDto>> findNotice(@PageableDefault(size = 15) Pageable pageable,
+                                                            @RequestParam(required = false, value = "q") String q);
+
+
+    @Operation(summary = "공지사항 세부 조회", description = "공지사항 세부 내용을 조회합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "공지사항 세부 조회",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CommonResponse.class),
+                                    examples = @ExampleObject(
+                                            value = """
+                                                    {
+                                                        "customCode": "NOTICE-SUCCESS-005",
+                                                        "customMessage": "공지사항 세부정보 조회 성공",
+                                                        "status": true,
+                                                        "data": {
+                                                            "title": "타이틀",
+                                                            "content": "내용",
+                                                            "authorNick": "테스트닉"
+                                                        }
+                                                    }
+                                                    """
+                                    ))),
+                    @ApiResponse(responseCode = "400", description = "해당 공지사항을 찾을 수 없음",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CommonResponse.class),
+                                    examples = {
+                                            @ExampleObject(
+                                                    value = """
+                                                            {
+                                                                "customCode": "NOTICE-ERR-001",
+                                                                "customMessage": "해당 공지사항을 찾을 수 없음",
+                                                                "status": false,
+                                                                "data": null
+                                                            }
+                                                            """
+                                            )
+                                    })),
+                    @ApiResponse(responseCode = "404", description = "해당 회원을 찾을 수 없음",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CommonResponse.class),
+                                    examples = {
+                                            @ExampleObject(
+                                                    value = """
+                                                            {
+                                                                "customCode": "MEMBER-ERR-003",
+                                                                "customMessage": "해당 회원을 찾을 수 없음",
+                                                                "status": false,
+                                                                "data": null
+                                                            }
+                                                            """
+                                            )
+                                    }))
+            })
+    public CommonResponse<NoticeDetailDto> findNoticeDetail(@PathVariable("notice_id") Long noticeId);
 
 
     @Operation(summary = "공지사항 수정", description = "공지사항을 수정합니다.",
