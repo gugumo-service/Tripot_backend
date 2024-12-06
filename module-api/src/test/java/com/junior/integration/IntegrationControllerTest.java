9dbf655c6e3922799cbb489a549d1ee997d16003
TheperZ <ckdgh7884@gmail.com>
2024-12-06 18:40:19 +0900

    member 관련 순수 스프링부트 통합테스트 작성

---
+++ module-api/src/test/java/com/junior/integration/IntegrationControllerTest.java
@@ -0,0 +1,59 @@
+package com.junior.integration;
+
+import com.junior.config.SecurityConfig;
+import com.junior.domain.member.Member;
+import com.junior.domain.member.MemberRole;
+import com.junior.domain.member.MemberStatus;
+import com.junior.domain.member.SignUpType;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = "spring.profiles.active=prod")
+@Transactional
+@Import(SecurityConfig.class)
+public class IntegrationControllerTest {
+
+    Member createPreactiveTestMember() {
+        return Member.builder()
+                .id(1L)
+                .nickname("테스트비활성화닉네임")
+                .username("테스트비활성화유저네임")
+                .role(MemberRole.USER)
+                .signUpType(SignUpType.KAKAO)
+                .profileImage("s3.com/testProfile")
+                .recommendLocation("서울")
+                .build();
+    }
+
+    Member createActiveTestMember() {
+        return Member.builder()
+                .id(2L)
+                .nickname("테스트사용자닉네임")
+                .username("테스트사용자유저네임")
+                .role(MemberRole.USER)
+                .signUpType(SignUpType.KAKAO)
+                .profileImage("s3.com/testProfile")
+                .recommendLocation("서울")
+                .status(MemberStatus.ACTIVE)
+                .build();
+    }
+
+    MockMultipartFile createMockMultipartFile() {
+        MockMultipartFile profileImg = new MockMultipartFile(
+                "프로필 사진",
+                "profiles.png",
+                MediaType.IMAGE_PNG_VALUE,
+                "thumbnail".getBytes()
+        );
+
+        return profileImg;
+
+    }
+}
