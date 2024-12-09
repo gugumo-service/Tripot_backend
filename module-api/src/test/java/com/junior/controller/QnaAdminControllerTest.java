836ee8ee5ebf6c18b42890b4eb3bf652b7ed1649
TheperZ <ckdgh7884@gmail.com>
2024-12-10 00:54:19 +0900

    Q&A 관련 컨트롤러 코드 작성
    - QnaAdminController
    - QnaUserController
    - QnaAdminControllerTest
    - QnaUserControllerTest
    - QnaUserApi
    - QnaAdminApi

---
+++ module-api/src/test/java/com/junior/controller/QnaAdminControllerTest.java
@@ -0,0 +1,233 @@
+package com.junior.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.junior.config.SecurityConfig;
+import com.junior.dto.notice.NoticeAdminDto;
+import com.junior.dto.qna.CreateQnaDto;
+import com.junior.dto.qna.QnaAdminDto;
+import com.junior.dto.qna.QnaDetailDto;
+import com.junior.dto.qna.UpdateQnaDto;
+import com.junior.page.PageCustom;
+import com.junior.security.JwtUtil;
+import com.junior.security.WithMockCustomAdmin;
+import com.junior.security.exceptionhandler.CustomAuthenticationEntryPoint;
+import com.junior.service.qna.QnaAdminService;
+import com.junior.service.security.UserDetailsServiceImpl;
+import com.junior.util.RedisUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(QnaAdminController.class)
+@MockBean(JpaMetamodelMappingContext.class)     //JPA 관련 빈들을 mock으로 등록
+@Import(SecurityConfig.class)
+class QnaAdminControllerTest {
+
+    @MockBean
+    private RedisUtil redisUtil;
+
+    @MockBean
+    private JwtUtil jwtUtil;
+
+    @MockBean
+    private UserDetailsServiceImpl userDetailsService;
+
+    @MockBean
+    private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private QnaAdminService qnaAdminService;
+
+    @InjectMocks
+    private QnaAdminController qnaAdminController;
+
+    @Test
+    @DisplayName("Q&A 저장 응답이 반환되어야 함")
+    @WithMockCustomAdmin
+    void saveQna() throws Exception {
+
+        //given
+        CreateQnaDto createQnaDto = new CreateQnaDto("question", "answer");
+        String content = objectMapper.writeValueAsString(createQnaDto);
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                post("/api/v1/admin/qna")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+        );
+
+
+        //then
+        actions
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.customCode").value("Q&A-SUCCESS-001"))
+                .andExpect(jsonPath("$.customMessage").value("Q&A 업로드 성공"))
+                .andExpect(jsonPath("$.status").value(true))
+                .andExpect(jsonPath("$.data").value(nullValue()));
+
+
+    }
+
+    @Test
+    @DisplayName("Q&A 조회 응답이 반환되어야 함")
+    @WithMockCustomAdmin
+    void findQna() throws Exception {
+
+        //given
+        Pageable resultPageable = PageRequest.of(0, 15);
+        String q = "";
+
+        List<QnaAdminDto> result = new ArrayList<>();
+
+        result.add(new QnaAdminDto(1L, "question"));
+
+
+        given(qnaAdminService.findQna(anyString(), any(Pageable.class)))
+                .willReturn(new PageCustom<>(result, resultPageable, result.size()));
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                get("/api/v1/admin/qna")
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+
+        //then
+        actions
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.customCode").value("Q&A-SUCCESS-004"))
+                .andExpect(jsonPath("$.customMessage").value("Q&A 조회 성공"))
+                .andExpect(jsonPath("$.status").value(true))
+                .andExpect(jsonPath("$.data.pageable.number").value(1))
+                .andExpect(jsonPath("$.data.content[0].question").value("question"));
+
+
+
+
+
+
+
+    }
+
+    @Test
+    @DisplayName("Q&A 세부내용 조회 응답이 반환되어야 함")
+    @WithMockCustomAdmin
+    void findQnaDetail() throws Exception {
+
+        //given
+
+        QnaDetailDto qnaDetailDto = QnaDetailDto.builder()
+                .id(1L)
+                .question("question")
+                .answer("answer")
+                .build();
+
+        given(qnaAdminService.findQnaDetail(anyLong())).willReturn(qnaDetailDto);
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                get("/api/v1/admin/qna/{qna_id}", 1L)
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+
+        //then
+        actions
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.customCode").value("Q&A-SUCCESS-005"))
+                .andExpect(jsonPath("$.customMessage").value("Q&A 세부정보 조회 성공"))
+                .andExpect(jsonPath("$.status").value(true))
+                .andExpect(jsonPath("$.data.question").value("question"))
+                .andExpect(jsonPath("$.data.answer").value("answer"));
+
+
+
+
+
+    }
+
+
+
+    @Test
+    @DisplayName("Q&A 수정 응답이 반환되어야 함")
+    @WithMockCustomAdmin
+    void updateQna() throws Exception {
+
+        //given
+        Long qnaId = 1L;
+        UpdateQnaDto updateQnaDto = new UpdateQnaDto("new question", "new answer");
+        String content = objectMapper.writeValueAsString(updateQnaDto);
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                patch("/api/v1/admin/qna/{qna_id}", qnaId)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+        );
+
+        //then
+        actions
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.customCode").value("Q&A-SUCCESS-003"))
+                .andExpect(jsonPath("$.customMessage").value("Q&A 수정 성공"))
+                .andExpect(jsonPath("$.status").value(true))
+                .andExpect(jsonPath("$.data").value(nullValue()));
+    }
+
+    @Test
+    @DisplayName("Q&A 삭제 응답이 반환되어야 함")
+    @WithMockCustomAdmin
+    void deleteQna() throws Exception {
+
+        //given
+        Long qnaId = 1L;
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                delete("/api/v1/admin/qna/{qna_id}", qnaId)
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+
+        //then
+        actions
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.customCode").value("Q&A-SUCCESS-002"))
+                .andExpect(jsonPath("$.customMessage").value("Q&A 삭제 성공"))
+                .andExpect(jsonPath("$.status").value(true))
+                .andExpect(jsonPath("$.data").value(nullValue()));
+    }
+}
---
+++ module-api/src/test/java/com/junior/controller/QnaUserControllerTest.java
@@ -0,0 +1,105 @@
+package com.junior.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.junior.config.SecurityConfig;
+import com.junior.dto.qna.QnaUserDto;
+import com.junior.security.JwtUtil;
+import com.junior.security.WithMockCustomUser;
+import com.junior.security.exceptionhandler.CustomAuthenticationEntryPoint;
+import com.junior.service.qna.QnaUserService;
+import com.junior.service.security.UserDetailsServiceImpl;
+import com.junior.util.RedisUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
+@WebMvcTest(QnaUserController.class)
+@MockBean(JpaMetamodelMappingContext.class)     //JPA 관련 빈들을 mock으로 등록
+@Import(SecurityConfig.class)
+class QnaUserControllerTest {
+
+    @MockBean
+    private RedisUtil redisUtil;
+
+    @MockBean
+    private JwtUtil jwtUtil;
+
+    @MockBean
+    private UserDetailsServiceImpl userDetailsService;
+
+    @MockBean
+    private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private QnaUserService qnaUserService;
+
+    @InjectMocks
+    private QnaUserController qnaUserController;
+
+
+    @Test
+    @DisplayName("사용자 Q&A 조회 응답이 반환되어야 함")
+    @WithMockCustomUser
+    void findQna() throws Exception {
+
+        //given
+
+        Long cursorId = 1L;
+        Integer size = 5;
+
+        PageRequest pageable = PageRequest.of(0, size);
+
+        List<QnaUserDto> result = new ArrayList<>();
+
+        result.add(new QnaUserDto(1L, "question", "answer", LocalDateTime.MIN));
+
+        given(qnaUserService.findQna(cursorId, size)).willReturn(new SliceImpl<>(result, pageable, false));
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                get("/api/v1/qna")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .param("cursorId", cursorId.toString())
+                        .param("size", size.toString())
+        );
+
+
+        //then
+        actions
+                .andDo(print())
+                .andExpect(jsonPath("$.customCode").value("Q&A-SUCCESS-004"))
+                .andExpect(jsonPath("$.customMessage").value("Q&A 조회 성공"))
+                .andExpect(jsonPath("$.status").value(true))
+                .andExpect(jsonPath("$.data.pageable.pageNumber").value(0))
+                .andExpect(jsonPath("$.data.pageable.pageSize").value(5))
+                .andExpect(jsonPath("$.data.content[0].question").value("question"));
+
+
+    }
+}
