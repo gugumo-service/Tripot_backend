c9e48b1470a1e048d8e325216c56695f45e8cdee
heeseong <donald010@naver.com>
2025-01-26 19:00:42 +0900

    단일 스토리 조회 수정
    단일 스토리 조회 시 비회원도 볼 수 있도록 수정

--- module-domain/src/main/java/com/junior/repository/story/StoryRepository.java
+++ module-domain/src/main/java/com/junior/repository/story/StoryRepository.java
@@ -5,7 +5,9 @@ import com.junior.repository.story.custom.StoryCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StoryRepository extends JpaRepository<Story, Long>, StoryCustomRepository {
-
+    Optional<Story> findByIdAndIsDeletedFalse(Long id);
 }
